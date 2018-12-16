a0cf1ab138466a02a493be4d39eaaab50ad5afb5
Josep Egea <jes@josepegea.com>
2018-12-16 17:31:29 +0100

    Refactor Xformer code to its own class

--- termux.rb
+++ termux.rb
@@ -14,10 +14,7 @@ module RubyTermux
       JSON.parse(api_command(command, args), symbolize_names: true)
     end
 
-    def typed_json_api_command(command, type_conversions = {}, *args)
-      res = json_api_command(command, args)
-      apply_types(res, type_conversions)
-    end
+    # Systems
 
     def clipboard
       @clipboard ||= Clipboard.new(self)
@@ -29,49 +26,6 @@ module RubyTermux
 
     def call_log
       @call_log ||= CallLog.new(self)
-    end
-
-    private
-
-    def apply_types(results, type_conversions)
-      collection = results.is_a?(Array) ? results : [results]
-      collection.each { |c| apply_types_to_item(c, type_conversions) }
-      results
-    end
-
-    def apply_types_to_item(item, type_conversions)
-      return unless item.is_a?(Hash)
-      type_conversions.each do |k, v|
-        next unless item.key?(k)
-        item[k] = apply_type_to_item_prop(item[k], v)
-      end
-    end
-
-    def apply_type_to_item_prop(value, type)
-      case type
-      when :date
-        Date.parse(value)
-      when :time
-        Time.parse(value)
-      when :duration
-        to_duration(value)
-      when :integer
-        value.to_i
-      when :float
-        value.to_f
-      when :symbol
-        value.to_sym
-      else
-        value
-      end
-    end
-
-    def to_duration(value)
-      parts = value.split(':').reverse
-      res = parts.pop.to_i
-      res += (parts.pop.to_i * 60) if parts.any?
-      res += (parts.pop.to_i * 60 * 60) if parts.any?
-      res
     end
   end
 
@@ -104,7 +58,53 @@ module RubyTermux
       args = []
       args += ['-l', limit.to_s] unless limit.nil?
       args += ['-o', offset.to_s] unless offset.nil?
-      base_object.typed_json_api_command('call-log', { date: :time, duration: :duration, type: :symbol }, *args)
+      res = base_object.json_api_command('call-log', *args)
+      Xformer.xform(res, date: :time, duration: :duration, type: :symbol)
+    end
+  end
+
+  class Xformer
+    def self.xform(object, args)
+      collection = object.is_a?(Array) ? object : [object]
+      collection.each { |i| xform_item(i, args) }
+      object
+    end
+
+    private
+
+    def self.xform_item(item, args)
+      return unless item.is_a?(Hash)
+      args.each do |arg, type|
+        next unless item.key?(arg)
+        item[arg] = xform_prop(item[arg], type)
+      end
+    end
+
+    def self.xform_prop(value, type)
+      case type
+      when :date
+        Date.parse(value)
+      when :time
+        Time.parse(value)
+      when :duration
+        to_duration(value)
+      when :integer
+        value.to_i
+      when :float
+        value.to_f
+      when :symbol
+        value.to_sym
+      else
+        value
+      end
+    end
+
+    def self.to_duration(value)
+      parts = value.split(':').reverse
+      res = parts.pop.to_i
+      res += (parts.pop.to_i * 60) if parts.any?
+      res += (parts.pop.to_i * 60 * 60) if parts.any?
+      res
     end
   end
 end
